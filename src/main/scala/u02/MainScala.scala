41d0f0ba418fb6bfd39f4506e6a028b06afe08f1
Claudio Verazza <claudio.verazza@studio.unibo.it>
2024-03-16 12:42:25 +0100

    Solution until ex 6

--- src/main/scala/u02/MainScala.scala
+++ src/main/scala/u02/MainScala.scala
@@ -1,6 +1,7 @@
 package u02
 
 object MainScala extends App:
+    /*Exercise 3*/
     def positiveMethod(x: Int): String = x match
         case x if x >= 0 => "positive"
         case _ => "negative"
@@ -23,13 +24,47 @@ object MainScala extends App:
         case false => true
 
     def positiveIntTest(x: Int): Boolean = x > 0
+
+    /*Exercise 4*/
+    val p1: Int => Int => Int => Boolean = x => y => z => x <= y && y == z
+    val p2: (Int, Int, Int) => Boolean = (x, y, z) => x <= y && y == z
+    def p3(x: Int)(y: Int)(z: Int): Boolean = x <= y && y == z
+    def p4(x: Int, y: Int, z: Int) : Boolean = x <= y && y == z
+
+    /*Exercise 5*/
+
+    def compose[A](f: A => A, g: A => A): A => A = i => f(g(i))
+    val inc: (i: Int) => Int = i => i + 5
+    val append: (s: String) => String = s => s concat "."
+
+    /*val funComposition1 = compose(append, append)
+    val funComposition2 = compose(inc, inc)
+    println(funComposition1("10"))
+    println(funComposition2(10))*/
+
+
+    /*Exercise 6*/
+
+    def gcd(a: Int, b: Int): Int = a match
+        case a if a == 0 => b
+        case a if b == 0 => a
+        case _ => gcd(b, a % b)
+        //case _ => a
+    
+    println(gcd(8, 12))
+    println(gcd(11, 3))
+
     /*println(positiveMethod(1))
     println(positiveLambda(-11))*/
     /*val notEmpty = neg(empty)
     println(notEmpty("ciao"))
     val notEmptyDef = negDef(emptyDef)
     println(notEmptyDef(""))*/
-    val negativeIntGenericTest = genericNeg(positiveIntTest)
+    /*val negativeIntGenericTest = genericNeg(positiveIntTest)
     println(negativeIntGenericTest(-1))
     val notEmptyStringGenericTest = genericNeg(empty)
-    println(notEmptyStringGenericTest("Hello"))
+    println(notEmptyStringGenericTest("Hello"))*/
+    /*println(p1(10)(10)(10))
+    println(p2(10, 10, 20))*/
+    /* println(p3(10)(10))
+    println(p4(10, 10, 10))*/
